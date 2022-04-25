3a7ad6805a2557432978bae0c640543003f94607
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-04-25 21:54:47 +0300

    ios fonts added

--- src/navigation/bottomTabNav.js
+++ src/navigation/bottomTabNav.js
@@ -6,43 +6,47 @@ import SearchScreen from '../screens/SearchScreen';
 import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNav = () => {
   return (
-    <Tab.Navigator
-    screenOptions={({route}) => ({
-      tabBarIcon: ({focused, color, size}) => {
-        let iconName;
-        switch (route.name) {
-          case 'home':
-            iconName = focused ? 'home' : 'home-outline';
-            break;
-          case 'search':
-            iconName = focused ? 'search' : 'search-outline';
-            break;
-          case 'leaf':
-            iconName = focused ? 'leaf' : 'leaf-outline';
-            break;
-          case 'profile':
-            iconName = focused ? 'user' : 'user-o';
-            return <FontAwesome name={iconName} size={size} color={color} />;
-          default:
-            break;
-        }
-        return <Ionicons name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: '#448014',
-      tabBarInactiveTintColor: '#32502E',
-      tabBarShowLabel: false,
-      headerShown: false
-    })} >
-      <Tab.Screen name="home" component={HomeScreen} />
-      <Tab.Screen name="leaf" component={PlantsScreen} />
-      <Tab.Screen name="search" component={SearchScreen} />
-      <Tab.Screen name="profile" component={ProfileScreen} />
-    </Tab.Navigator>
+
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            switch (route.name) {
+              case 'home':
+                iconName = focused ? 'home' : 'home-outline';
+                break;
+              case 'search':
+                iconName = focused ? 'search' : 'search-outline';
+                break;
+              case 'leaf':
+                iconName = focused ? 'leaf' : 'leaf-outline';
+                break;
+              case 'profile':
+                iconName = focused ? 'user' : 'user-o';
+                return <FontAwesome name={iconName} size={size} color={color} />;
+              default:
+                break;
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#448014',
+          tabBarInactiveTintColor: '#32502E',
+          tabBarShowLabel: false,
+          headerShown: false
+        })} >
+        <Tab.Screen name="home" component={HomeScreen} />
+        <Tab.Screen name="leaf" component={PlantsScreen} />
+        <Tab.Screen name="search" component={SearchScreen} />
+        <Tab.Screen name="profile" component={ProfileScreen} />
+      </Tab.Navigator>
+
+
   );
 }
 
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabNav from './bottomTabNav';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen
-          component={BottomTabNav}
-          name="HomeTabs"
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            component={BottomTabNav}
+            name="HomeTabs"
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </SafeAreaView>
+
+
   );
 };
 
