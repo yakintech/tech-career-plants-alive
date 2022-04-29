a2c2610a76024dbe283dcef24c4a1af2e2b0de05
gulcinozpmk <84877089+gulcinozpmk@users.noreply.github.com>
2022-04-29 21:27:12 +0300

    Register sayfasının tasarımı eklendi.

--- src/screens/ProfileScreen/RegisterScreen.js
+++ src/screens/ProfileScreen/RegisterScreen.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Button, StyleSheet, TextInput, View, Text } from 'react-native';
+import { Button, StyleSheet, TextInput, View, Text, Image, ImageBackground } from 'react-native';
 import { Formik } from 'formik';
 import * as yup from 'yup'
 import baseManager from '../../api/baseManager';
+
 
 const RegisterScreen = ({ navigation }) => {
 
@@ -32,60 +33,79 @@ const RegisterScreen = ({ navigation }) => {
     }
 
     return (
+        <>
+            <View style={styles.index}>
+                <Image
+                    source={require('../../../assets/data/img/logoRegister.png')}
+                    style={styles.logo}
+                >
+                </Image>
+                <Formik
+                    validationSchema={registerValidationSchema}
+                    initialValues={{ email: '', name: '', surname: '' }}
+                    onSubmit={values => register(values)}
 
-        <Formik
-            validationSchema={registerValidationSchema}
-            initialValues={{ email: '', name: '', surname: '' }}
-            onSubmit={values => register(values)}
-        >
-            {({ handleChange, handleSubmit, values, errors }) => (
-                <View>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={handleChange('email')}
-                        value={values.email}
-                        placeholder='EMail'
-                        autoCapitalize='none'
-                    />
-                    {errors.email &&
-                        <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
-                    }
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={handleChange('name')}
-                        value={values.name}
-                        placeholder='Name'
+                >
+                    {({ handleChange, handleSubmit, values, errors }) => (
+                        <View flex={1}>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={handleChange('email')}
+                                value={values.email}
+                                placeholder='Email (e.g: abc@abc.com)'
+                                autoCapitalize='none'
+                            />
+                            {errors.email &&
+                                <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
+                            }
 
-                    />
-                    {errors.name &&
-                        <Text style={{ fontSize: 10, color: 'red' }}>{errors.name}</Text>
-                    }
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={handleChange('surname')}
-                        value={values.surname}
-                        placeholder='Surname'
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={handleChange('name')}
+                                value={values.name}
+                                placeholder='Name'
 
-                    />
-                    {errors.name &&
-                        <Text style={{ fontSize: 10, color: 'red' }}>{errors.name}</Text>
-                    }
-                    <Button onPress={handleSubmit} title="Submit" />
-                </View>
-            )}
-        </Formik>
+                            />
+                            {errors.name &&
+                                <Text style={{ fontSize: 10, color: 'red' }}>{errors.name}</Text>
+                            }
 
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={handleChange('surname')}
+                                value={values.surname}
+                                placeholder='Surname'
 
+                            />
+
+                            {errors.surname &&
+                                <Text style={{ fontSize: 10, color: 'red', paddingBottom: 10 }}>{errors.surname}</Text>
+                            }
+                            <Button onPress={handleSubmit} title="Submit" color="#04541d" />
+                        </View>
+                    )}
+                </Formik>
+            </View>
+        </>
     )
 }
 
 const styles = StyleSheet.create({
+    index: {
+        flex: 1,
+        backgroundColor: '#fff4de',
+    },
     input: {
         height: 40,
+        width: 370,
         margin: 12,
         borderWidth: 1,
         padding: 10,
     },
+    logo: {
+        height: 200,
+        width: 400,
+    }
 });
 
 export default RegisterScreen
